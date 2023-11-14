c30d343ebed0f253bb0119736244573ed0e69179
Charlie Stanard <charliestanard@guildeducation.com>
2023-11-14 14:02:30 -0600

    Formatting and add useful key mappings

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
--------------------------------------
--- Simple, barebones neovim config --
--------------------------------------
+-----------------------------------------------------------
+-- Simple, barebones neovim config
+-----------------------------------------------------------
 
 -- Init lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -16,10 +16,18 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Load plugins
+-----------------------------------------------------------
+-- Plugins
+-----------------------------------------------------------
+
 require('lazy').setup(
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 )
+
+-----------------------------------------------------------
+-- Configuration settings
+-- https://neovim.io/doc/user/options.html
+-----------------------------------------------------------
 
 -- Color scheme
 -- Additional configuration options https://github.com/catppuccin/nvim
@@ -37,3 +45,28 @@ vim.opt.hlsearch = false    -- highlight results of previous search
 vim.opt.ignorecase = true   -- ignore uppercase letters when executing search
 vim.opt.incsearch = true    -- highlight pattern matches while typing search
 vim.opt.smartcase = true    -- make search ignore uppercase letters unless the search term has uppercase
+
+-----------------------------------------------------------
+-- Key mappings
+-----------------------------------------------------------
+
+-- Disable arrow keys
+map('', '<up>', '<nop>')
+map('', '<down>', '<nop>')
+map('', '<left>', '<nop>')
+map('', '<right>', '<nop>')
+
+-- Move around splits using Ctrl + {h,j,k,l}
+map('n', '<C-h>', '<C-w>h', {desc = 'Focus window left'})
+map('n', '<C-j>', '<C-w>j', {desc = 'Focus window down'})
+map('n', '<C-k>', '<C-w>k', {desc = 'Focus window up'})
+map('n', '<C-l>', '<C-w>l', {desc = 'Focus window right'})
+
+-- Map 'esc' to kk
+map('i', 'kk', '<Esc>')
+-- Quit
+map('n', '<leader>qq', ':qa!<cr>')
+-- Write buffer
+map('n', '<leader>w', ':w<cr>')
+-- Write all buffers
+map('n', '<leader>ww', ':wa<cr>')
