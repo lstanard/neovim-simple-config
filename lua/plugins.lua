a1d535f3ba0871ece81d593572c9b73d27ceba34
Charlie Stanard <charlie.stanard@guildeducation.com>
2023-12-06 19:50:41 -0600

    Add auto-complete with github copilot

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -207,9 +207,19 @@ require('lazy').setup({
     'nvim-telescope/telescope-file-browser.nvim',
     dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   },
-  -- GitHub Copilot
+  -- Copilot
   {
-    'github/copilot.vim'
+    'zbirenbaum/copilot.lua',
+    config = function()
+      require('copilot').setup()
+    end
+  },
+  -- Copilot for nvim-cmp
+  {
+    'zbirenbaum/copilot-cmp',
+    config = function ()
+      require('copilot_cmp').setup()
+    end
   },
   -- Syntax parser
   {
@@ -284,6 +294,19 @@ require('lazy').setup({
     config = function()
       require('fidget').setup({})
     end,
+  },
+  -- Auto-complete & snippets
+  'rafamadriz/friendly-snippets',
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
   },
 })
 
@@ -383,3 +406,77 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
   end,
 })
+
+-----------------------------------------------------------
+-- LSP Config: nvim-cmp
+-----------------------------------------------------------
+
+local status, cmp = pcall(require, 'cmp')
+local luasnip = require('luasnip')
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip").filetype_extend("javascript", { "javascriptreact" })
+
+luasnip.config.setup {}
+
+if (not status) then return end
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete({}),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
+    }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = "copilot", group_index = 2 },
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'luasnip' },
+    { name = 'path' },
+  }),
+})
+
+-- [Reference](https://github.com/windwp/nvim-autopairs#you-need-to-add-mapping-cr-on-nvim-cmp-setupcheck-readmemd-on-nvim-cmp-repo)
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
+
+-- Set up lspconfig
+local lspConfigCapabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig')['tsserver'].setup {
+  capabilities = lspConfigCapabilities
+}
+
+vim.cmd [[
+  highlight! default link CmpItemKind CmpItemMenuDefault
+]]
